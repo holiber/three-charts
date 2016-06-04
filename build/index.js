016907b11cb5203e3192ddb9cb20834a2ea9e0ce
holiber <kadabrik91@gmail.com>
2016-06-04 16:48:58 +0300

    new build settings

---
+++ build/index.js
@@ -0,0 +1,16 @@
+var index =
+webpackJsonp_name_([1],[
+/* 0 */
+/***/ function(module, exports, __webpack_require__) {
+
+	"use strict";
+	function __export(m) {
+	    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+	}
+	/// <reference path="typings/index.d.ts" />
+	__export(__webpack_require__(1));
+
+
+/***/ }
+]);
+//# sourceMappingURL=index.js.map
---
+++ build/typings/index.d.ts
@@ -0,0 +1,6 @@
+/// <reference path="globals/eventemitter2/index.d.ts" />
+/// <reference path="globals/greensock/index.d.ts" />
+/// <reference path="globals/stats/index.d.ts" />
+/// <reference path="globals/three/index.d.ts" />
+/// <reference path="globals/webpack-env/index.d.ts" />
+/// <reference path="modules/es6-promise/index.d.ts" />
