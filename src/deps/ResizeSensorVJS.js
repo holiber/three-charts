12a4dda783eb88a9a26907a7e42bda91caca9d5a
holiber <kadabrik91@gmail.com>
2016-09-23 15:09:43 +0300

    fix main file in package.json

--- src/deps/ResizeSensorVJS.js
+++
@@ -1,8 +0,0 @@
-
-// module.exports = {
-//     ResizeSensor: require('css-element-queries/src/ResizeSensor')
-// }
-
-var ResizeSensor = require('css-element-queries/src/ResizeSensor')
-
-module.exports = ResizeSensor;
