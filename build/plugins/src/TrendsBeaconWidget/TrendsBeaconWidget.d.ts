2111f2dd7fb36f68ee9121083726eacd84192698
holiber <kadabrik91@gmail.com>
2016-11-03 20:22:07 +0300

    refactor Screen

---
+++ build/plugins/src/TrendsBeaconWidget/TrendsBeaconWidget.d.ts
@@ -0,0 +1,29 @@
+import Mesh = THREE.Mesh;
+import { Chart, TrendsWidget, TrendWidget, TrendSegmentsManager, ITrendOptions } from 'three-charts';
+/**
+ * widget adds blinking beacon on trends end
+ * activated when trend.hasBeacon = true
+ */
+export declare class TrendsBeaconWidget extends TrendsWidget<TrendBeacon> {
+    static widgetName: string;
+    protected getTrendWidgetClass(): typeof TrendBeacon;
+}
+export declare class TrendBeacon extends TrendWidget {
+    private mesh;
+    private animated;
+    private segment;
+    private animation;
+    static widgetIsEnabled(trendOptions: ITrendOptions): boolean;
+    constructor(chart: Chart, trendName: string);
+    getObject3D(): Mesh;
+    onTrendChange(): void;
+    protected bindEvents(): void;
+    private initObject();
+    private animate();
+    private setInitialState();
+    onDestroy(): void;
+    private static createTexture();
+    protected onTransformationFrame(): void;
+    protected onSegmentsAnimate(trendsSegments: TrendSegmentsManager): void;
+    private updatePosition();
+}
